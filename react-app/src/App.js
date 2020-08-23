c130e7ffef36e8ed7f89c3ec1b9d959d12375c5f
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-08-24 00:48:12 +0500

    error handle with sweet alert

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
+import swal from 'sweetalert';
 
 const List = props =>
   (
     <tr>
       <td>{props.value.name}</td>
-      <td> <a href={'mailto:'+props.value.email} target="blank"> {props.value.email} </a> </td>
+      <td> <a href={'mailto:' + props.value.email} target="blank"> {props.value.email} </a> </td>
       <td>{props.value.phone}</td>
     </tr>
   )
@@ -23,7 +24,7 @@ export default class App extends Component {
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/users')
       .then(res => this.setState({ userList: res.data }))
-      .catch(err => console.log(err))
+      .catch(err => swal("Opps!", "Error Occur", "error"))
   }
 
   UserList = () => {
