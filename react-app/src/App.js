13cc004372fb45163f854ec62615c3cf11a87ee7
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-08-24 00:40:37 +0500

    get all users fake api integrated

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const List = props =>
+  (
+    <tr>
+      <td>{props.value.name}</td>
+      <td>{props.value.email}</td>
+      <td>{props.value.phone}</td>
+    </tr>
+  )
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userList: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => this.setState({ userList: res.data }))
+      .catch(err => console.log(err))
+  }
+
+  UserList = () => {
+    return this.state.userList.map((item, i) => {
+      return <List value={item} key={i} />
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand">
+            <span style={{ fontWeight: '800' }}>USERLIST With REACT JS</span>
+          </a>
+        </nav>
+        <table className="table mt-5">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.UserList()
+            }
+          </tbody>
+        </table>
+
+      </div>
+    );
+  }
 }
 
-export default App;
