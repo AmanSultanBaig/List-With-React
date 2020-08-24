e094cbf33d241b35561e1071247254fb94236a37
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-08-24 14:30:10 +0500

    view profile api integrated

--- react-app/src/components/ViewProfile.js
+++ react-app/src/components/ViewProfile.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import axios from 'axios'
+import swal from 'sweetalert'
 class ViewProfile extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userProfile: {},
+            userAddress: {}
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/users/' + this.props.match.params.id)
+            .then(userDetails => {
+                this.setState({ userProfile: userDetails.data })
+                this.setState({ userAddress: userDetails.data.address })
+                console.log(userDetails.data)
+            }
+            )
+            .catch(err => swal("Opps!", err.message, "error"))
+    }
 
     render() {
         return (
             <div className="card mt-5" style={{ width: '100%' }}>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="float-right btn btn-primary">Go somewhere</a>
+                    <h3 className="card-title">{this.state.userProfile.name} <span style={{ fontSize: '10px' }}> <b>Email</b> ({this.state.userProfile.email}), <b>UserName</b> ({this.state.userProfile.username}) </span></h3>
+                    <div className="row mt-5">
+                        <p className="card-text col-md-4"><b>Phone:</b> {this.state.userProfile.phone}</p>
+                        <p className="card-text col-md-4"><b>City:</b> {this.state.userAddress.city}</p>
+                        <p className="card-text col-md-4"> <b>Street:</b> {this.state.userAddress.street}</p>
+                    </div>
+                    <a href="/" className="float-right btn btn-dark">Go Back</a>
                 </div>
             </div>
         )
